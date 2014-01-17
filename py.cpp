5ceaff04e71f9357f074341c23c1dab028afb6b6
Jalen Lyu <jalen.lyu@gmail.com>
2014-01-17 17:53:07 +0800

    use +/- and digit to select word

--- py.cpp
+++ py.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char * argv[]){
     pinyin_context_t * context =
@@ -14,50 +15,67 @@ int main(int argc, char * argv[]){
 
     pinyin_instance_t * instance = pinyin_alloc_instance(context);
 
-    char * prefixbuf = NULL; size_t prefixsize = 0;
-    char * linebuf = NULL; size_t linesize = 0;
+    char * prefixbuf = NULL;
+    size_t prefixsize = 0;
+    char * linebuf = NULL;
+    size_t linesize = 0;
     ssize_t read;
 
-    while( TRUE ){
-        fprintf(stdout, "prefix:");
-        fflush(stdout);
-
-        if ((read = getline(&prefixbuf, &prefixsize, stdin)) == -1)
-            break;
-
-        if ( '\n' == prefixbuf[strlen(prefixbuf) - 1] ) {
-            prefixbuf[strlen(prefixbuf) - 1] = '\0';
-        }
-
+    while(TRUE) {
+        //if ( '\n' == prefixbuf[strlen(prefixbuf) - 1] ) {
+        //    prefixbuf[strlen(prefixbuf) - 1] = '\0';
+        //}
         fprintf(stdout, "pinyin:");
         fflush(stdout);
 
         if ((read = getline(&linebuf, &linesize, stdin)) == -1)
             break;
 
-        if ( '\n' == linebuf[strlen(linebuf) - 1] ) {
+        if ('\n' == linebuf[strlen(linebuf) - 1]) {
             linebuf[strlen(linebuf) - 1] = '\0';
         }
 
-        if ( strcmp ( linebuf, "quit" ) == 0)
+        if (strcmp(linebuf, "quit") == 0)
             break;
 
         pinyin_parse_more_full_pinyins(instance, linebuf);
-        pinyin_guess_sentence_with_prefix(instance, prefixbuf);
+        //pinyin_guess_sentence_with_prefix(instance, prefixbuf);
         pinyin_guess_full_pinyin_candidates(instance, 0);
 
         guint len = 0;
+        guint group_len = 5;
+        const char *words[5];
+        guint word_index = 0;
         pinyin_get_n_candidate(instance, &len);
-        for (size_t i = 0; i < len; ++i) {
-            lookup_candidate_t * candidate = NULL;
-            pinyin_get_candidate(instance, i, &candidate);
+        if (len == 0) continue;
+        guint group_num = len / group_len + ((len % group_len > 0) ? 1 : 0);
 
-            const char * word = NULL;
-            pinyin_get_candidate_string(instance, candidate, &word);
-
-            printf("%d:%s\t", (int)i, word);
-        }
-        printf("\n");
+        guint i = 0;
+        // operation
+        char c;
+        do {
+            for (guint j = 0; j < group_len; j++) {
+                guint index = i * group_len + j;
+                if (index >= len) break;
+                lookup_candidate_t * candidate = NULL;
+                pinyin_get_candidate(instance, i * group_len + j, &candidate);
+                const char * word = NULL;
+                pinyin_get_candidate_string(instance, candidate, &word);
+                words[j] = word;
+                printf("%u: %s\t", (int)j, word);
+            }
+            printf("\noperation(+/- or digit): ");
+            c = getchar();
+            // strip the enter key
+            if (c != '\n') getchar();
+            if (c == '+' && i < group_num) {
+                ++i;
+            } else if (c == '-' && i > 0) {
+                --i;
+            } else if (isdigit(c)) {
+                printf("select: %s\n", words[(int)(c - '0')]);
+            }
+        } while (isdigit(c) || c == '+' || c == '-');
 
         pinyin_train(instance);
         pinyin_reset(instance);
