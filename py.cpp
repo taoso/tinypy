07663c293b88aa9c04a340e096135866ac09258a
Jalen Lyu <jalen.lyu@gmail.com>
2014-01-17 18:20:05 +0800

    split libpinyin init and free into function

--- py.cpp
+++ py.cpp
@@ -4,22 +4,23 @@
 #include <string.h>
 #include <ctype.h>
 
+struct py {
+    pinyin_context_t* context;
+    pinyin_instance_t* instance;
+};
+
+void py_init(struct py * p, char *sys_data_path, char *user_data_path);
+void py_free(struct py *p);
+
 int main(int argc, char * argv[]){
-    pinyin_context_t * context =
-        pinyin_init("/usr/lib/libpinyin/data", "./data");
-
-    pinyin_option_t options =
-        PINYIN_CORRECT_ALL | USE_DIVIDED_TABLE | USE_RESPLIT_TABLE |
-        DYNAMIC_ADJUST;
-    pinyin_set_options(context, options);
-
-    pinyin_instance_t * instance = pinyin_alloc_instance(context);
-
     char * prefixbuf = NULL;
     size_t prefixsize = 0;
     char * linebuf = NULL;
     size_t linesize = 0;
     ssize_t read;
+
+    struct py p;
+    py_init(&p, NULL, NULL);
 
     while(TRUE) {
         //if ( '\n' == prefixbuf[strlen(prefixbuf) - 1] ) {
@@ -38,15 +39,15 @@ int main(int argc, char * argv[]){
         if (strcmp(linebuf, "quit") == 0)
             break;
 
-        pinyin_parse_more_full_pinyins(instance, linebuf);
+        pinyin_parse_more_full_pinyins(p.instance, linebuf);
         //pinyin_guess_sentence_with_prefix(instance, prefixbuf);
-        pinyin_guess_full_pinyin_candidates(instance, 0);
+        pinyin_guess_full_pinyin_candidates(p.instance, 0);
 
         guint len = 0;
         guint group_len = 5;
         const char *words[5];
         guint word_index = 0;
-        pinyin_get_n_candidate(instance, &len);
+        pinyin_get_n_candidate(p.instance, &len);
         if (len == 0) continue;
         guint group_num = len / group_len + ((len % group_len > 0) ? 1 : 0);
 
@@ -58,9 +59,9 @@ int main(int argc, char * argv[]){
                 guint index = i * group_len + j;
                 if (index >= len) break;
                 lookup_candidate_t * candidate = NULL;
-                pinyin_get_candidate(instance, i * group_len + j, &candidate);
+                pinyin_get_candidate(p.instance, i * group_len + j, &candidate);
                 const char * word = NULL;
-                pinyin_get_candidate_string(instance, candidate, &word);
+                pinyin_get_candidate_string(p.instance, candidate, &word);
                 words[j] = word;
                 printf("%u: %s\t", (int)j, word);
             }
@@ -77,17 +78,34 @@ int main(int argc, char * argv[]){
             }
         } while (isdigit(c) || c == '+' || c == '-');
 
-        pinyin_train(instance);
-        pinyin_reset(instance);
-        pinyin_save(context);
+        pinyin_train(p.instance);
+        pinyin_reset(p.instance);
+        pinyin_save(p.context);
     }
 
-    pinyin_free_instance(instance);
-
-    pinyin_mask_out(context, 0x0, 0x0);
-    pinyin_save(context);
-    pinyin_fini(context);
-
-    free(prefixbuf); free(linebuf);
+    py_free(&p);
+    free(prefixbuf);
+    free(linebuf);
     return 0;
+}
+
+void py_init(struct py * p, char *sys_data_path, char *user_data_path)
+{
+    if (!sys_data_path) sys_data_path = (char *)"/usr/lib/libpinyin/data";
+    if (!user_data_path) user_data_path = (char *)"./data";
+    p->context = pinyin_init("/usr/lib/libpinyin/data", "./data");
+
+    pinyin_option_t options = PINYIN_CORRECT_ALL | USE_DIVIDED_TABLE
+        | USE_RESPLIT_TABLE | DYNAMIC_ADJUST;
+    pinyin_set_options(p->context, options);
+    p->instance = pinyin_alloc_instance(p->context);
+}
+
+void py_free(struct py *p)
+{
+    pinyin_free_instance(p->instance);
+
+    pinyin_mask_out(p->context, 0x0, 0x0);
+    pinyin_save(p->context);
+    pinyin_fini(p->context);
 }
